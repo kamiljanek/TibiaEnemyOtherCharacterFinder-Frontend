f27a58f18cb6cc8de966b12cb7b4ed30b1810e4d
Kamil Janek <kjanek@unitygroup.com>
2024-01-06 14:19:53 +0100

    Remove default styles

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,54 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Logo from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import { CharacterResult } from "./types/CharacterResult";
+import Character from "./components/Character";
+import { SearchBar } from "./components/SearchBar";
 
 function App() {
+  const result: CharacterResult = {
+    name: "Bobeek",
+    world: "Bona",
+    vocation: "Elder Druid",
+    level: 2482,
+    lastLogin: new Date("2024-01-04 13:05:41"),
+    formerNames: [],
+    formerWorlds: [],
+    traded: false,
+    otherVisibleCharacters: ["Bobeek", "Bobekx", "Lidera Bobek"],
+    possibleInvisibleCharacters: [
+      {
+        name: "lidera bobek",
+        numberOfMatches: 271,
+        firstMatchDate: new Date("2022-12-06"),
+        lastMatchDate: new Date("2024-01-03"),
+      },
+      {
+        name: "bobek here",
+        numberOfMatches: 22,
+        firstMatchDate: new Date("2023-05-10"),
+        lastMatchDate: new Date("2023-12-29"),
+      },
+      {
+        name: "elzena lisy",
+        numberOfMatches: 12,
+        firstMatchDate: new Date("2022-12-06"),
+        lastMatchDate: new Date("2023-02-08"),
+      },
+    ],
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <Logo />
+      <div>
+        <SearchBar></SearchBar>
+      </div>
+      <Main />
+      <Character>{result}</Character>
+      {/* <Footer /> */}
     </div>
   );
 }
