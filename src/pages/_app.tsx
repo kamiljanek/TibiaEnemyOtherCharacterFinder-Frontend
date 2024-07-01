b72d73ec7e21d5e65bf55558b2f7340eb8e49094
Andrzej Pacholik <andrzej.pacholik@gmail.com>
2024-07-01 12:08:37 +0200

    Switching to NextJS

---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { PageContainer } from "../components/PageContainer";
+import MainContainer from "../components/MainContainer";
+import FooterContainer from "../components/FooterContainer";
+import { Row } from "react-bootstrap";
+
+function App() {
+  return (
+    <PageContainer>
+      <Row style={{ flexGrow: 1, alignItems: "center" }}>
+        <MainContainer />
+      </Row>
+      <Row>
+        <FooterContainer />
+      </Row>
+    </PageContainer>
+  );
+}
+
+export default App;
