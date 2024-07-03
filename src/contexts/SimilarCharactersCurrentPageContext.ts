5415822104dccb743a9f751b44ffab7d2c795590
Andrzej Pacholik <andrzej.pacholik@gmail.com>
2024-07-03 18:55:52 +0200

    fix a some linter errors

--- src/contexts/SimilarCharactersCurrentPageContext.ts
+++ src/contexts/SimilarCharactersCurrentPageContext.ts
@@ -1,3 +1,4 @@
 import { createContext, Dispatch, SetStateAction } from "react";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 export const SimilarCharactersCurrentPageContext = createContext<[number, Dispatch<SetStateAction<number>>]>([1, () => {}]);
